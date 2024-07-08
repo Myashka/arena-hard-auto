d7b3332d061daf9e76a9b05992045692e826e99a
Alexey <porsh-5@yandex.ru>
2024-07-08 17:34:52 +0300

    + RS stop

--- utils.py
+++ utils.py
@@ -112,7 +112,7 @@ def chat_completion_openai(model, messages, temperature, max_tokens, api_dict=No
                 messages=messages,
                 temperature=temperature,
                 max_tokens=max_tokens,
-                stop=["<|end_of_text|>", "<|eot_id|>"]
+                stop=["<|end_of_text|>", "<|eot_id|>", "</RS>"]
                 )
             output = completion.choices[0].message.content.strip()
             break
